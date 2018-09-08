721dcdb17938cf3062dd0907b11ed7b205778b98
Geoyeob Kim <gasbank@msn.com>
2018-09-08 17:40:01 +0900

    서버에서 클라이언트 별 값 할당이 가능한지 테스트 중

--- Assets/Scripts/Server/HandlePlayerList.cs
+++ Assets/Scripts/Server/HandlePlayerList.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UniWebServer;
 using System.Linq;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(EmbeddedWebServerComponent))]
 public class HandlePlayerList : MonoBehaviour, IWebResource
@@ -26,16 +27,44 @@ public class HandlePlayerList : MonoBehaviour, IWebResource
     {
         public string userId;
         public int score;
+        public int prop1;
+        public int prop2;
     }
 
     public void HandleRequest(Request request, Response response)
     {
         response.statusCode = 200;
         response.message = "OK.";
-        var playerList = new PlayerListResponse
+        foreach (var player in PhotonNetwork.playerList)
         {
-            playerList = PhotonNetwork.playerList.Select(e => new Player { userId = e.UserId, score = e.GetScore() }).ToArray()
-        };
-        response.Write(JsonUtility.ToJson(playerList));
+            Debug.LogFormat("User ID: {0}", player.UserId);
+        }
+        List<Player> playerList = new List<Player>();
+        foreach (var player in PhotonNetwork.playerList)
+        {
+            var p = new Player
+            {
+                userId = player.UserId,
+                score = player.GetScore()
+            };
+            if (player.CustomProperties != null)
+            {
+                object prop1;
+                if (player.CustomProperties.TryGetValue("prop1", out prop1))
+                {
+                    p.prop1 = (int)prop1;
+                }
+                object prop2;
+                if (player.CustomProperties.TryGetValue("prop2", out prop2))
+                {
+                    p.prop2 = (int)prop2;
+                }
+            }
+            playerList.Add(p);
+        }
+        Debug.Log(playerList);
+        Debug.Log(playerList.Count);
+        
+        response.Write(JsonUtility.ToJson(new PlayerListResponse { playerList = playerList.ToArray() }));
     }
 }
--- Assets/Scripts/Server/Server.cs
+++ Assets/Scripts/Server/Server.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Server : MonoBehaviour
+public class Server : Photon.MonoBehaviour
 {
     void Start()
     {
@@ -21,19 +21,40 @@ public class Server : MonoBehaviour
     void OnJoinedRoom()
     {
         Debug.LogFormat("OnJoinedRoom(): Player User ID = {0}", PhotonNetwork.player.UserId);
+        var cp = PhotonNetwork.player.CustomProperties;
+        cp.Add("prop1", 1985);
+        PhotonNetwork.player.SetCustomProperties(cp);
+        cp = PhotonNetwork.player.CustomProperties;
+        cp.Add("prop2", 19852);
+        PhotonNetwork.player.SetCustomProperties(cp);
         foreach (var player in PhotonNetwork.playerList)
         {
+            
             Debug.LogFormat("User ID: {0}", player.UserId);
         }
     }
 
     void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
     {
+        var cp = newPlayer.CustomProperties;
+        cp.Add("prop1", 9999999);
+        newPlayer.SetCustomProperties(cp);
         Debug.LogFormat("OnPhotonPlayerConnected(): Player User ID = {0}", PhotonNetwork.player.UserId);
         foreach (var player in PhotonNetwork.playerList)
         {
             //player.GetScore();
             Debug.LogFormat("User ID: {0}", player.UserId);
+        }
+
+        if (photonView != null)
+        {
+            
+            photonView.RPC("SetCustomProp", newPlayer, 100);
+            Debug.Log("photonView custom prop set!");
+        }
+        else
+        {
+            Debug.Log("photonView null!");
         }
     }
 
